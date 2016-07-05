d4fbfee2b3e8899de002c87ecf42bc9cd94d83dc
jiangtaows <taoJiang_ict@163.com>
2016-07-05 18:07:58 +0800

    version 3.0 加入英文词典

--- spell check.py
+++ spell check.py
@@ -1,3 +1,4 @@
+#-*- coding: UTF-8 -*-
 class quick_sort():
     a=[]
     def __init__(self,data):
@@ -80,6 +81,67 @@ def make_trie(words):
         last_word=word+''
         last_length=len(word)
     return buff
+def make_trie_from_txtFile(filename):
+    print "---->Begin read words from File"
+    f=open(filename)
+    words=[]
+    word=f.readline()
+    while word:
+        words.append(word[:-1])
+        word=f.readline()
+    f.close()
+    print "---->Finish read words from File"
+
+    print "---->Begin sort words"
+    sort=quick_sort(words)
+    #sort.show()
+    words=sort.sort(1,len(words)-1)
+    #sort.show()
+    print "---->Finish sort words:"
+    print "---->Begin make trie"
+    #initiate
+    buff=[]
+    pointer=0
+    last_word=['' for i in range(50)]
+    last_word_letter_index_information=[0 for i in range(50)]
+    last_length=0
+
+    buff.append(['',0,0])
+    pointer=0
+    last_word=''
+    last_word_letter_index_information[0]=0
+    last_length=1
+    #run
+    for word in words:
+        if word!=last_word and word!='':#去词典重复的单词 读的词项中有''空字符，与结束符冲突
+            length=len(word)
+            t=min(length,last_length)
+            while last_word[:t]!=word[:t]:
+                t-=1
+            #first
+            brother=last_word_letter_index_information[t]
+            buff.append([word[t],0,0])
+            pointer+=1
+            buff[brother][2]=pointer #brother
+            last_word_letter_index_information[t]=pointer
+            parent=pointer
+            t+=1
+            while t<length:
+                buff.append([word[t],0,0])
+                pointer+=1
+                buff[parent][1]=pointer #child
+                last_word_letter_index_information[t]=pointer
+                parent=pointer
+                t+=1
+            #last : ''
+            buff.append(['',0,0])
+            pointer+=1
+            buff[parent][1]=pointer #child
+            last_word_letter_index_information[t]=pointer
+
+            last_word=word+''
+            last_length=len(word)
+    return buff
 def look(word,trie):
     pointer=0
     exist=True
@@ -100,5 +162,8 @@ def look(word,trie):
 
 
 words=['a','ad','ec','z','bc']
-look('',make_trie(words))
+trie=make_trie_from_txtFile("english_words.txt")
+look('andtt',trie)
+look('ands',trie)
+look('finds',trie)
 
